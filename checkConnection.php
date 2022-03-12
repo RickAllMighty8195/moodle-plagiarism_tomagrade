f71adcb36e8d2c7d11eda2597f365d8f6c09aa13
RickAllMighty8195 <96168420+RickAllMighty8195@users.noreply.github.com>
2022-03-12 15:27:23 +0200

    Update checkConnection.php

--- checkConnection.php
+++ checkConnection.php
@@ -1,51 +1,52 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+    // This file is part of Moodle - https://moodle.org/
+    //
+    // Moodle is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // Moodle is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-/**
- * checkConnection.php - Checks the connection using ApiKeys
- *
- * @package    plagiarism_tomagrade
- * @subpackage plagiarism
- * @copyright  2021 Tomax ltd <roy@tomax.co.il>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+    /**
+     * checkConnection.php - Checks the connection using ApiKeys
+     *
+     * @package    plagiarism_tomagrade
+     * @subpackage plagiarism
+     * @copyright  2021 Tomax ltd <roy@tomax.co.il>
+     * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+     */
 
-require_once(dirname(dirname(__FILE__)) . '/../config.php');
-require_once($CFG->libdir . '/adminlib.php');
-require_once($CFG->libdir . '/plagiarismlib.php');
-require_once($CFG->dirroot . '/plagiarism/tomagrade/lib.php');
-require_once($CFG->dirroot . '/plagiarism/tomagrade/plagiarism_form.php');
-require_login();
+    require_once(dirname(dirname(__FILE__)) . '/../config.php');
+    require_once($CFG->libdir . '/adminlib.php');
+    require_once($CFG->libdir . '/plagiarismlib.php');
+    require_once($CFG->dirroot . '/plagiarism/tomagrade/lib.php');
+    require_once($CFG->dirroot . '/plagiarism/tomagrade/plagiarism_form.php');
+    require_login();
 
-defined('MOODLE_INTERNAL') || die();
-global $DB, $CFG;
+    defined('MOODLE_INTERNAL') || die();
+    global $DB, $CFG;
 
-$connection = new tomagrade_connection;
-$res = $connection->get_courses();
-if (isset($res)) {
-    if ($res["IsTokenActive"] == true) {
-        write("Your system is well connected!");
+    $connection = new tomagrade_connection;
+    $res = $connection->get_courses();
+    if (isset($res)) {
+        if ($res["IsTokenActive"] == true) {
+            write("Your system is well connected!");
+        } else {
+            write("Please check your APIKey and UserID.");
+        }
     } else {
-        write("Please check your APIKey and UserID.");
+        write("It seems you do not have an open connection to TomaGrade");
     }
-} else {
-    write("It seems you do not have an open connection to TomaGrade");
-}
-echo ("<script>window.close();</script>");
+    echo ("<script>window.close();</script>");
 
-function write($message) {
-    echo ("<script>alert('$message');</script>");
-}
+    function write($message) {
+        echo ("<script>alert('$message');</script>");
+    }
+?>
